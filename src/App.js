a9b1698519f6a77cca266652f5f48d7d613b5d1c
AhoySmallFry <jordana@makin.co>
2021-11-20 16:13:46 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AhoyLogo from "./ahoy-logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={AhoyLogo} className="App-logo" alt="logo" />
       </header>
+      <p>Hello World!</p>
     </div>
   );
 }
