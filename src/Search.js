203fe1f8c34d657b83f1367759f66905c2b0adfd
AhoySmallFry <jordana@makin.co>
2021-12-02 17:41:23 +0000

    Added image API

--- src/Search.js
+++ src/Search.js
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 import "./Search.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Search(props) {
   console.log(props.defaultSearchTerm);
   let [searchTerm, setsearchTerm] = useState(props.defaultSearchTerm);
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function LookUp(event) {
@@ -17,6 +23,12 @@ export default function Search(props) {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000012b45059f0783470abcc1c68ff4eb2f29";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${searchTerm}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSearchTermChange(event) {
@@ -34,6 +46,9 @@ export default function Search(props) {
       </form>
       <div className="card">
         <Results data={results} />
+      </div>
+      <div className="photos">
+        <Photos photos={photos} />
       </div>
     </div>
   );
