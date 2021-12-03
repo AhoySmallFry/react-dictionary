f78b8cc4c11ec82cdf40c12f24fbcc2910a219d1
AhoySmallFry <jordana@makin.co>
2021-12-03 14:57:36 +0000

    Removed synonym heading

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,7 +4,6 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <div className="Synonyms">
-        Synonyms:{" "}
         {props.synonyms.map(function (synonym, index) {
           return (
             <span key={index} className="synonyms">
